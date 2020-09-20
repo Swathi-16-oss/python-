c9684d6e3da9bdafe3bd4c95d8749de897604516
Devarasetty Swathi <70877614+Swathi-16-oss@users.noreply.github.com>
2020-09-20 17:50:34 +0530

    Create stack.py
    create stack class and then insert element to stack by using append() method to add elements /items
    if you what to delete the item then use pop() method
    peeks() is the last element in stack .

---
+++ stack.py
@@ -0,0 +1,30 @@
+class stack():
+    def __init__(self):
+        self.ele=[]
+    def push(self,new):
+        self.ele.append(new)
+    def pop(self):
+        return self.ele.pop()
+    def isempty(self):
+        return self.ele==[]
+    def peek(self):
+        if not self.isempty():
+            return self.ele[-1]
+    def getstack(self):
+        return self.ele
+
+s=stack()
+print(s.isempty())
+s.push("s")
+s.push("w")
+s.push("a")
+s.push("t")
+s.push("h")
+s.push("i")
+s.push("'s")
+print(s.getstack())
+s.pop()
+print(s.isempty())
+
+print(s.getstack())
+print(s.peek())
