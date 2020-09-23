f852cae73875dec9012a2e6f52fdf2d0cb057063
Devarasetty Swathi <70877614+Swathi-16-oss@users.noreply.github.com>
2020-09-23 18:12:49 +0530

    Create queue1.py
    in python we have inbuilt module deque for queues .
    so that so can add and remove easily in queue .use append to insert element and popleft() is used to remove the element from left index in queue.

---
+++ queue1.py
@@ -0,0 +1,13 @@
+from collections import deque
+que=deque()
+que.append('D')
+que.append('s')
+que.append('w')
+que.append('a')
+que.append('t')
+que.append('h')
+que.append('i')
+print(que)
+print(que.popleft())
+
+print(que)
