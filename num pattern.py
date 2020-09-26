a2c1cb94e721b86a6746136c75fa3a19fbb8b174
Devarasetty Swathi <70877614+Swathi-16-oss@users.noreply.github.com>
2020-09-26 23:46:01 +0530

    Create num pattern.py

---
+++ num pattern.py
@@ -0,0 +1,22 @@
+for i in range(5):
+    print("") #to print new line
+    
+    for j in range(5):
+        print(i+1,end="")#end to print new line
+''' 11111
+    22222
+    33333
+    44444
+    55555'''
+
+for i in range(5):#for rows
+    print("")
+    for j in range(i+1):
+        print(j+1,end="")#for numbers
+ 
+''' 1
+    12
+    123
+    1234
+    12345'''
+  
