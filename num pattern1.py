af0aced56f3afbfe83a33648c6e6b807ad514d83
Devarasetty Swathi <70877614+Swathi-16-oss@users.noreply.github.com>
2020-09-27 17:57:10 +0530

    Create num pattern1.py

---
+++ num pattern1.py
@@ -0,0 +1,24 @@
+for i in range(6):
+    print("")
+    for i in range(0,i,1):
+        print(2**i, end=' ')
+'''o/p
+1 
+1 2 
+1 2 4 
+1 2 4 8 
+1 2 4 8 16'''
+
+for i in range(6):
+    print("")
+    for i in range(0,i,1):
+        print(2**i, end=' ')
+    for i in range(-1+i,-1,-1):
+        print(2**i,end=" ")
+
+'''o/p
+1 
+1 2 1 
+1 2 4 2 1 
+1 2 4 8 4 2 1 
+1 2 4 8 16 8 4 2 1 '''
