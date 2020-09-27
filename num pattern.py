f7f088d6ad9ac2fd73cc72b273ba3032a95c2db0
Devarasetty Swathi <70877614+Swathi-16-oss@users.noreply.github.com>
2020-09-27 17:09:10 +0530

    Update num pattern.py

--- num pattern.py
+++ num pattern.py
@@ -3,7 +3,8 @@ for i in range(5):
     
     for j in range(5):
         print(i+1,end="")#end to print new line
-''' 11111
+'''o/p
+    11111
     22222
     33333
     44444
@@ -14,9 +15,32 @@ for i in range(5):#for rows
     for j in range(i+1):
         print(j+1,end="")#for numbers
  
-''' 1
+''' o/p
+    1
     12
     123
     1234
     12345'''
   
+for i in range(5):#for rows
+    print("")
+    for j in range(i+1):
+        j=i
+        print(j+1,end="")#for numbers
+'''o/p
+1
+22
+333
+4444
+55555'''
+
+for i in range(5,0,-1):
+    print("")
+    for j in range(1,i+1):
+        print(j,end="")
+'''o/p
+12345
+1234
+123
+12
+1'''
