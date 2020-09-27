a1c955c818a36cb414b73e5e9e337c6965fd088f
Devarasetty Swathi <70877614+Swathi-16-oss@users.noreply.github.com>
2020-09-27 17:59:29 +0530

    Update num pattern1.py

--- num pattern1.py
+++ num pattern1.py
@@ -13,7 +13,7 @@ for i in range(6):
     print("")
     for i in range(0,i,1):
         print(2**i, end=' ')
-    for i in range(-1+i,-1,-1):
+    for i in range(i-1,-1,-1):
         print(2**i,end=" ")
 
 '''o/p
