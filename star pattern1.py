f7f6b37743c316fe0e9fe1500e0f975728561fa5
Devarasetty Swathi <70877614+Swathi-16-oss@users.noreply.github.com>
2020-09-29 16:47:16 +0530

    Update star pattern1.py

--- star pattern1.py
+++ star pattern1.py
@@ -12,6 +12,18 @@ for i in range(6):
     * * * 
   * * * * 
 * * * * * '''
+for i in range(6,-1,-1):
+    for j in range(i):
+        print("*", end=" ")
+    print("")
+
+'''o/p:
+* * * * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* '''
 k=5
 for i in range(6):
     for j in range(k):
@@ -27,5 +39,52 @@ for i in range(6):
   * * * 
  * * * * 
 * * * * * '''
+
+k=10
+for i in range(5,-1,-1):
+    for j in range(k,0,-1):
+        print("",end=" ")
+    k=k+1
+    for j in range(i+1):
+        print("*", end=" ")
+    print("")
+'''o/p:
+          * * * * * * 
+           * * * * * 
+            * * * * 
+             * * * 
+              * * 
+               * '''
+
+k=15
+for i in range(5):
+    for j in range(k):
+        print("",end=" ")
+    k=k-1
+    for j in range(i+1):
+        print("*", end=" ")
+    print("")
+k=10
+for i in range(5,-1,-1):
+    for j in range(k,0,-1):
+        print("",end=" ")
+    k=k+1
+    for j in range(i+1):
+        print("*", end=" ")
+    print("")
+'''o/p:
+               * 
+              * * 
+             * * * 
+            * * * * 
+           * * * * * 
+          * * * * * * 
+           * * * * * 
+            * * * * 
+             * * * 
+              * * 
+               * 
+
+
 
 
