221a07ef0cdcb834f2116a136895da540b49a7f5
Devarasetty Swathi <70877614+Swathi-16-oss@users.noreply.github.com>
2020-10-01 23:46:33 +0530

    Create celsius to fahrenheit.py

---
+++ celsius to fahrenheit.py
@@ -0,0 +1,19 @@
+
+celsius = 37.5
+# calculate fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
+'''o/p:
+37.5 degree Celsius is equal to 99.5 degree Fahrenheit'''
+  
+  
+  
+  
+celsius=float(input("enter the temperature in celsius"))
+# calculate fahrenheit
+fahrenheit = float(celsius * 1.8) + 32
+print("'%0.1f'fahrenheit" %(fahrenheit))
+
+'''o/p:
+enter the temperature in celsius37.5
+'99.5'fahrenheit'''
