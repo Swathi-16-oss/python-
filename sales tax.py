3bec06f64ac4a1d09bf4eec28801d46a56a25549
Devarasetty Swathi <70877614+Swathi-16-oss@users.noreply.github.com>
2020-10-06 23:37:23 +0530

    Create sales tax.py

---
+++ sales tax.py
@@ -0,0 +1,16 @@
+'''calculating sales tax:
+ cost of item*sales tax of rate in decimal part=sales tax
+
+ 
+ calculating price of an item:
+ cost of item+sales tax=total cost of item'''
+
+itemcost=60
+salestax=7.5
+salestax=7.5/100
+total=itemcost*salestax
+print("%.2f"%total)
+
+#o/p:4.50
+
+ 
