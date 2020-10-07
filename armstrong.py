f81f6875c5c1383cbc59fccf1ca2e6ad886c1a00
Devarasetty Swathi <70877614+Swathi-16-oss@users.noreply.github.com>
2020-10-07 23:09:28 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -2,18 +2,32 @@ list=[]
 list1=[]
 total=0
 b=0
-n=int(input("enter number"))
+n=int(input("enter number of digits"))
 for i in range(1,n+1):
-    ele=int(input("enter the number"))
+    ele=int(input("enter the digit"))
     list1.append(ele)
     a=(ele**n)
     list.append(a)
 for j in range(0,len(list)):
                total=total+list[j]
 
+
 b=int(''.join(str(k) for k in list1))
 if(b==total):
     print(b,"is armstrong")
 else:
     print(b,"is not armstrong")
-
+'''o/p:
+enter number of digits3
+enter the digit4
+enter the digit0
+enter the digit4
+404 is not armstrong
+>>> 
+>>> 
+enter number of digits3
+enter the digit4
+enter the digit0
+enter the digit7
+407 is armstrong
+>>> '''
