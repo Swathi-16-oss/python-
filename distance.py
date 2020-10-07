ef95a1eb8e53e553b397d607b3c882ad79a03e2e
Devarasetty Swathi <70877614+Swathi-16-oss@users.noreply.github.com>
2020-10-07 22:37:03 +0530

    Update distance.py

--- distance.py
+++ distance.py
@@ -1,5 +1,11 @@
 time = int(input("enter time"))
 speed = int(input("enter speed"))
 distance = speed* time
-print('\nThe distance the car will travel in', time, 'hours =', distance)
+print('\nThe distance covered within ', time, 'hours =', distance)
 
+
+'''o/p:
+enter time4
+enter speed5
+
+The distance covered within  4 hours = 20'''
