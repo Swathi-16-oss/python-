3c7204dd383ae8fccd0c20d5e5b5896d648cba5d
Devarasetty Swathi <70877614+Swathi-16-oss@users.noreply.github.com>
2020-10-09 23:43:13 +0530

    Create sum of n natural number.py

---
+++ sum of n natural number.py
@@ -0,0 +1,23 @@
+n=int(input("enter number"))
+sum=0
+for i in range(n+1):
+    sum=sum+i
+print("sum of {}  natural number is {}".format(n,sum))
+'''o/p:
+enter number15
+sum of 15  natural number is 120'''
+
+
+
+n=int(input("enter number"))
+sum=0
+i=1
+while(n>=i):
+    sum=sum+i
+    i=i+1
+print("sum of {}  natural number is {}".format(n,sum))
+'''o/p:
+enter number15
+sum of 15  natural number is 120'''
+
+
