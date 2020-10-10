e74b59d81b7daa086275d392be8c9ae368d16845
Devarasetty Swathi <70877614+Swathi-16-oss@users.noreply.github.com>
2020-10-10 21:35:27 +0530

    Create binary 01&01.py
    in binary if it contains "10 "and "01" then print yes if not print no.

---
+++ binary 01&01.py
@@ -0,0 +1,41 @@
+n=int(input("enter number"))
+a=[]
+while(n>0):
+    r=n%2
+    a.append(r)
+    n=n//2
+s=str(''.join(str(i) for i in a))
+bin=s[::-1]
+print(bin)
+#for 10 it take 010 
+if(len(bin)==2 and bin.find("10")>-1):
+    print("yes")
+elif bin.find("01")>-1 and bin.find("10")>-1:
+    print("yes")
+else:
+    print("no")
+
+    
+'''o/p:
+enter number4
+100
+no
+>>> 
+===============
+enter number5
+101
+yes
+>>> 
+================
+enter number2
+10
+yes
+>>> 
+==================
+enter number3
+11
+no
+==== ==========
+enter number4
+100
+no'''
