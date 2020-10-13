fca7509d554d1146469364c594c458c21f9cfbd3
Devarasetty Swathi <70877614+Swathi-16-oss@users.noreply.github.com>
2020-10-14 00:31:16 +0530

    Update pangram.py

--- pangram.py
+++ pangram.py
@@ -1,1 +1,22 @@
 
+str=(input())
+str=str.lower()# to convert lowercase
+l=[]
+for ele in str:
+    if(ele!=" " and ele not in l):#to avoid duplicate 
+            l.append(ele)
+            
+            
+    else:
+        pass
+if(len(l)==26):
+    print("pangram")
+else:
+    print("not pangram")
+
+
+
+'''i/p:
+We promptly judged antique ivory buckles for the next prize
+o/p:
+pangram'''
