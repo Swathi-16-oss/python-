8c7365a8f71a728e3db96a53a96578c242d5b8fa
Devarasetty Swathi <70877614+Swathi-16-oss@users.noreply.github.com>
2020-11-10 22:20:02 +0530

    Update age calculation.py

--- age calculation.py
+++ age calculation.py
@@ -1,10 +1,9 @@
 from datetime import datetime
-dob = input("Enter date of birth dd/mm/yyyy format: ")
+dob = input("Enter date of birth dd/mm/yyyy format: ") #input is in the some of dd/mm/yyyy format
 dob = datetime.strptime(dob, '%d/%m/%Y')
 print(datetime.today())
 print ("Years %d"%((datetime.today() - dob).days/365))
 print(dob)
-
 
 ''o/p:
 Enter date of birth dd/mm/yyyy format: 29/6/1999
